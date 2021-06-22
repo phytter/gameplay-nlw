96e8c67a33f19fb1d9aa03c4931363b6b924d899
phytter <phytter@hotmail.com>
2021-06-21 23:41:26 -0300

    first commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  colors: {
+    background: '0E1647'
+  }
+}
---
+++ src/screens/Signin/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.background
+  },
+  input: {
+    height: 50,
+    width: 200,
+    borderBottomWidth: 2
+  }
+});
