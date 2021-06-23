8ff92f35eefe974fb50c29594bef1dcfd608f897
phytter <phytter@hotmail.com>
2021-06-22 21:12:27 -0300

    Finalizado aula 1

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   View,
   Text, 
   Image,
+  StatusBar
 } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
+import { ButtonIcon } from '../../components/ButtonIcon'
+
 export function SignIn(){
   
   return (
     <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       <Image
-        style={styles.image}
         source={IllustrationImg}
+        style={styles.image}
+        resizeMode='stretch'
       />
       <View style={styles.content}>
         <Text style={styles.title}>
@@ -27,6 +37,10 @@ export function SignIn(){
           Crie grupos para jogar seus games {`\n`}
           favoritos com amigos
         </Text>
+        <ButtonIcon
+         title='Entrar com Discord'
+         activeOpacity={0.7}
+        />
       </View>
     </View>
   );
